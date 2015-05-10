2a7e5e82035b97e072b510c47e550ca19d54553d
Tyler <TylerLiu@TylerdeMacBook-Pro.local>
2015-05-10 13:32:38 +0800

    Initial Commit

---
+++ Yummy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yummy
+//
+//  Created by Tyler on 15/5/10.
+//  Copyright (c) 2015年 Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yummy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yummy
+//
+//  Created by Tyler on 15/5/10.
+//  Copyright (c) 2015年 Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
